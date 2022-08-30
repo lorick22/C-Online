915da9fce239b2ca95d2c503980e4da427a072a8
harmse <harmse@LAB-2-PC4>
2022-08-30 08:16:05 +0200

    Add project files.

---
+++ Y-Online.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Y_Online.UI.MvcApplication" Language="C#" %>
---
+++ Y-Online.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Y_Online.UI.Startup))]
+namespace Y_Online.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
